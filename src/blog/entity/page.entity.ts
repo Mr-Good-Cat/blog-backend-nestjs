22759fe9355eccccdc27c76ec4df23d820f8ddf1
ivan.sankov <ivan.sankov@skillguilds.com>
2023-08-18 16:57:52 +0700

    page entity was created

---
+++ src/blog/entity/page.entity.ts
@@ -0,0 +1,40 @@
+import {
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import {PageType} from "../enum/page-type.enum";
+import {PageStatus} from "../enum/page-status.enum";
+
+@Entity()
+export class Page {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column({ type: "text" })
+    description: string;
+
+    @Column()
+    order: number;
+
+    @Column({ type: 'enum', enum: PageType })
+    type: PageType;
+
+    @Column({ name: 'seo_title' })
+    seoTitle: string;
+
+    @Column({ name: 'seo_description' })
+    seoDescription: string;
+
+    @Column('ltree', { nullable: true })
+    path: string;
+
+    @Column()
+    slug: string;
+
+    @Column({ type: 'enum', enum: PageStatus, default: PageStatus.PENDING })
+    status: PageStatus;
+}
---
+++ src/blog/enum/page-type.enum.ts
@@ -0,0 +1,5 @@
+export enum PageType {
+    MAIN_CATEGORY = 'MAIN_CATEGORY',
+    CATEGORY = 'CATEGORY',
+    ARTICLE = 'ARTICLE'
+}
