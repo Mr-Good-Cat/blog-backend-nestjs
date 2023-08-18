e651f9da7c3c87ff1a6c2b5ca95f8f855e647657
ivan.sankov <ivan.sankov@skillguilds.com>
2023-08-18 17:40:25 +0700

    response page dto was updated

--- src/blog/dto/response/page.dto.ts
+++ src/blog/dto/response/page.dto.ts
@@ -1,1 +1,38 @@
-export class PageDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { PageType } from '../../enum/page-type.enum';
+import { PageStatus } from '../../enum/page-status.enum';
+
+export class PageDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty()
+  order: number;
+
+  @ApiProperty({ enum: PageType })
+  type: PageType;
+
+  @ApiProperty()
+  seoTitle: string;
+
+  @ApiProperty()
+  seoDescription: string;
+
+  @ApiProperty()
+  slug: string;
+
+  @ApiProperty()
+  path: string;
+
+  @ApiProperty({ enum: PageStatus })
+  status: PageStatus;
+
+  @ApiProperty()
+  createAt: Date;
+}
