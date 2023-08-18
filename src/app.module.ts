30e3ab5587f1361819135a89e7dad76cb159b037
ivan.sankov <ivan.sankov@skillguilds.com>
2023-08-18 16:45:11 +0700

    default files was removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import config from './common/config/main.config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import {TypeOrmModule} from "@nestjs/typeorm";
 
@@ -20,7 +18,5 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 
     ConfigModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
